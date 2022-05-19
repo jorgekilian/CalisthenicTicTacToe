48d2a45be86e30cce943ae7fe1cb6e83944e31bc
Kilian Suárez <ksuarez@domingoalonsogroup.com>
2022-05-19 16:25:00 +0100

    player2_wins_the_match_horizontal_line

--- TicTacToe/Calisthenic/TicTacToeSpecs.cs
+++ TicTacToe/Calisthenic/TicTacToeSpecs.cs
@@ -263,5 +263,22 @@ namespace Calisthenic {
             // El estado es Player 1 Wins
             Assert.AreEqual(board.Status(), TicTacToeStatus.Player1Winner);
         }
+
+        [Test]
+        public void player2_wins_the_match_horizontal_line() {
+            var player1 = new Player("X");
+            var player2 = new Player("O");
+
+            // Realizo 6 jugadas 
+            board.Roll(player1, new Position(1, 0));
+            board.Roll(player2, new Position(0, 0));
+            board.Roll(player1, new Position(1, 1));
+            board.Roll(player2, new Position(0, 1));
+            board.Roll(player1, new Position(2, 2));
+            board.Roll(player2, new Position(0, 2));
+
+            // El estado es Player 2 Wins
+            Assert.AreEqual(board.Status(), TicTacToeStatus.Player2Winner);
+        }
     }
 }
